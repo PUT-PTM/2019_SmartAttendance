0b4160e677afa98689f1443fc4a610d382c224e1
Gunock <asz.czyk@gmail.com>
2019-04-26 23:42:59 +0200

    Add reusable popup button listener attachment function.

--- Server_Code/static/scripts/listeners.js
+++ Server_Code/static/scripts/listeners.js
@@ -2,30 +2,54 @@ $(document).ready(function () {
     $('#GitHub').on("click", function () {
         window.open('https://github.com/PUT-PTM/2019_SmartAttendance', '_blank');
     });
+
     $('#PP').on("click", function () {
         window.open('https://www.put.poznan.pl', '_blank');
     });
+
     $('#But_Data').on("click", function () {
         db_get_student_info();
         db_get_presence();
     });
-    $('#But_Add').on("click", function () {
-        let popups = $('.Popups_Container');
-        if (popups.is(':visible')) {
-            popups_fadeout();
-        } else {
-            popups_fadein('Add student', popups_inputs_add);
-        }
+
+    
+    function add_popup_listener(elemName, elemTitle, elemFunction)
+    {
+        $(elemName).on("click", function () {
+            let popups = $('.Popups_Container');
+            let title = String($('#Inputs_Title').html());
+
+            if (popups.is(':visible') && title === elemTitle) {
+                popups_fadeout();
+            } else {
+                if (title !== elemTitle) {
+                    popups.fadeOut(250, function () {
+                        popups_fadein('Add student', elemFunction)
+                    });
+                } else {
+                    popups_fadein('Add student', elemFunction);
+                }
+            }
+        });
+    }
+
+    const popup_buttons = [
+          ['#But_Add', 'Add stuudent', popups_inputs_add],
+          ['#But_Del', 'Delete stuudent', popups_inputs_delete]
+      ];
+
+    popup_buttons.forEach(function (entry) {
+        add_popup_listener(entry[0], entry[1], entry[2]);
     });
+
+
     $('#Cancel').on("click", function () {
         let popups = $('.Popups_Container');
         if (popups.is(':visible')) {
             popups_fadeout();
         }
     });
-    $('#But_Del').on("click", function () {
-        popups_fadein('Delete student', popups_inputs_delete);
-    });
+
     $('#But_Pres_Add').on("click", function () {
         let index = parseInt($('#Input_SID').val());
         let CID = parseInt($('#Input_CID').val());
